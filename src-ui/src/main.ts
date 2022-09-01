182160de1066a303ae337a50aca6c48f84c5c61b
GeckoEidechse <gecko.eidechse+git@pm.me>
2022-09-01 20:12:28 +0200

    Remove elements from template application

--- src-ui/src/main.ts
+++ src-ui/src/main.ts
@@ -14,9 +14,9 @@ var globalState = {
 
 document.addEventListener("DOMContentLoaded", async function () {
     // get the elements
-    const helloEl = $("div.hello")! as HTMLElement;
-    let counterButtonEl = $("counter-button") as HTMLElement;
-    let counterResultEl = $("counter-result") as HTMLElement;
+    // const helloEl = $("div.hello")! as HTMLElement;
+    // let counterButtonEl = $("counter-button") as HTMLElement;
+    // let counterResultEl = $("counter-result") as HTMLElement;
     let pingEl = $("backend-ping")! as HTMLElement;
     let panicButtonEl = $("panic-button") as HTMLElement;
     let installLocationHolderEl = $("install-location-holder") as HTMLElement;
@@ -84,20 +84,20 @@ document.addEventListener("DOMContentLoaded", async function () {
         alert("TODO");
     });
 
-    // counter button click
-    counterButtonEl.addEventListener("pointerup", async function () {
-        const result = await invoke("add_count", { num: 1 }) as string;
-        counterResultEl.textContent = result;
-    });
+    // // counter button click
+    // counterButtonEl.addEventListener("pointerup", async function () {
+    //     const result = await invoke("add_count", { num: 1 }) as string;
+    //     counterResultEl.textContent = result;
+    // });
 
-    // hello click
-    helloEl.addEventListener("pointerup", async function () {
-        const result = await invoke("hello_world") as string;
-        helloEl.textContent = result;
-        setTimeout(function () {
-            helloEl.textContent = "Click again";
-        }, 1000);
-    })
+    // // hello click
+    // helloEl.addEventListener("pointerup", async function () {
+    //     const result = await invoke("hello_world") as string;
+    //     helloEl.textContent = result;
+    //     setTimeout(function () {
+    //         helloEl.textContent = "Click again";
+    //     }, 1000);
+    // })
 
     // panic button click
     panicButtonEl.addEventListener("pointerup", async function () {
