3ab7ade87ba7fb7e6f8bb2184f707e2afa979a75
Remy Raes <raes.remy@gmail.com>
2022-09-22 23:04:26 +0200

    refactor: remove HelloWorld vue component

--- src-vue/src/App.vue
+++ src-vue/src/App.vue
@@ -1,7 +1,4 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -11,19 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <el-main>Main</el-main>
     </el-container>
   </div>
-  <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
