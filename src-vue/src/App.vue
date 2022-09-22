efcb9bb65295f331f321851a4b256af6159df8b8
Remy Raes <raes.remy@gmail.com>
2022-09-23 01:00:09 +0200

    feat: add some style to tabs

--- src-vue/src/App.vue
+++ src-vue/src/App.vue
@@ -21,6 +21,10 @@
   color: #898b8e;
   text-transform: uppercase;
   border: none !important;
+  font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', '微软雅黑', Arial, sans-serif;
+  font-weight: bold;
+  font-size: large;
+  margin: 10px 0;
 }
 
 .fc_menu__tabs .el-tabs__item:hover {
@@ -34,6 +38,7 @@
 /* Header menu */
 .fc_menu__tabs .el-tabs__header {
   background-color: #3c4549;
+  height: auto !important;
 }
 
 </style>
