b93afb828c341f883813c78650fdfee96d0fa907
Remy Raes <raes.remy@gmail.com>
2022-09-23 00:43:12 +0200

    feat: add basic tabs

--- src-vue/src/App.vue
+++ src-vue/src/App.vue
@@ -2,13 +2,38 @@
 </script>
 
 <template>
-  <div class="common-layout">
-    <el-container>
-      <el-header>Header</el-header>
-      <el-main>Main</el-main>
-    </el-container>
-  </div>
+  <el-tabs class="fc_menu__tabs" type="card">
+    <el-tab-pane label="Play">Play</el-tab-pane>
+    <el-tab-pane label="Changelog">Changelog</el-tab-pane>
+    <el-tab-pane label="Mods">Mods</el-tab-pane>
+    <el-tab-pane label="Settings">Settings</el-tab-pane>
+  </el-tabs>
 </template>
 
-<style scoped>
+<style>
+/* Borders reset */
+.fc_menu__tabs .el-tabs__nav, .fc_menu__tabs .el-tabs__header {
+  border: none !important;
+}
+
+/* Header item */
+.fc_menu__tabs .el-tabs__item {
+  color: #898b8e;
+  text-transform: uppercase;
+  border: none !important;
+}
+
+.fc_menu__tabs .el-tabs__item:hover {
+  color: #c6c9ce;
+}
+
+.fc_menu__tabs .is-active {
+  color: white !important;
+}
+
+/* Header menu */
+.fc_menu__tabs .el-tabs__header {
+  background-color: #3c4549;
+}
+
 </style>
