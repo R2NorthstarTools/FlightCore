21784ec3d747000c18b529b6569e1d6941a0c338
Remy Raes <raes.remy@gmail.com>
2022-09-23 18:02:43 +0200

    build: add element-plus-icons dependency

--- src-vue/src/main.ts
+++ src-vue/src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from "element-plus";
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 
 // styles
 import 'element-plus/theme-chalk/index.css';
@@ -8,4 +10,10 @@ import './style.css'
 
 const app = createApp(App);
 app.use(ElementPlus);
+
+// icons
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component);
+}
+
 app.mount('#app')
