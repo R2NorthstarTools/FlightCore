ba53e308a0908d0852e23d86efc66acf837485e4
Alystrasz <contact@remyraes.com>
2022-10-03 22:20:37 +0200

    feat: add panic button to dev view

--- src-vue/src/views/DeveloperView.vue
+++ src-vue/src/views/DeveloperView.vue
@@ -3,17 +3,32 @@
         <el-button type="primary" @click="disableDevMode">
             Disable developer mode
         </el-button>
+
+        <el-button type="primary" @click="crashApplication">
+            Panic button
+        </el-button>
     </div>
 </template>
 
 <script lang="ts">
 import {defineComponent} from "vue";
+import { invoke } from "@tauri-apps/api";
+import { ElNotification } from "element-plus";
 
 export default defineComponent({
     name: "DeveloperView",
     methods: {
         disableDevMode() {
             this.$store.commit('toggleDeveloperMode');
+        },
+        async crashApplication() {
+            await invoke("force_panic");
+            ElNotification({
+                title: 'Error',
+                message: "Never should have been able to get here!",
+                type: 'error',
+                position: 'bottom-right'
+            });
         }
     }
 });
