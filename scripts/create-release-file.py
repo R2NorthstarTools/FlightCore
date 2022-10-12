f36a6e4846b2400aa8ad22d82ff295cd507ed12e
GeckoEidechse <gecko.eidechse+git@pm.me>
2022-10-13 01:46:34 +0200

    Show link in update notification

--- scripts/create-release-file.py
+++ scripts/create-release-file.py
@@ -12,7 +12,7 @@ PATH_TO_LINUX_SIG = f"./artifact/appimage/flight-core_{version_number_stripped_v
 PATH_TO_WINDOWS_SIG = f"./artifact/msi/FlightCore_{version_number_stripped_v}_x64_en-US.msi.zip.sig"
 
 # Text to show in update notification
-RELEASE_TEXT = "Test version"
+RELEASE_TEXT = "See the following link for release notes: https://github.com/GeckoEidechse/FlightCore/releases"
 
 # Read signatures
 with open(PATH_TO_LINUX_SIG, "rt") as f:
