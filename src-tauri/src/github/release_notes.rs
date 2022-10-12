cfa89c8c0d84b7872e8a9de1302a0f2a3e9c79d7
Remy Raes <contact@remyraes.com>
2022-10-12 18:20:36 +0200

    feat: only transmit some info to frontend
    GitHub API gives much information about releases, we only need some: name,
    publication date and content of such release; so other information is not
    transmitted to UI.

--- src-tauri/src/github/release_notes.rs
+++ src-tauri/src/github/release_notes.rs
@@ -1,5 +1,15 @@
+use std::vec::Vec;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct ReleaseInfo {
+    pub name: String,
+    pub published_at: String,
+    pub body: String
+}
+
 #[tauri::command]
-pub fn get_northstar_release_notes() -> Result<String, String> {
+pub fn get_northstar_release_notes() -> Result<Vec<ReleaseInfo>, String> {
     println!("Fetching releases notes from GitHub API");
 
     let url = "https://api.github.com/repos/R2Northstar/Northstar/releases";
@@ -13,9 +23,13 @@ pub fn get_northstar_release_notes() -> Result<String, String> {
         .text()
         .unwrap();
 
-    let json_response: serde_json::Value =
+    let json_response: Vec<serde_json::Value> =
         serde_json::from_str(&res).expect("JSON was not well-formatted");
     println!("Done checking GitHub API");
     
-    return Ok(json_response.to_string());
+    return Ok(json_response.iter().map(|release| ReleaseInfo {
+        name: release.get("name").and_then(|value| value.as_str()).unwrap().to_string(),
+        published_at: release.get("published_at").and_then(|value| value.as_str()).unwrap().to_string(),
+        body: release.get("body").and_then(|value| value.as_str()).unwrap().to_string(),
+    }).collect());
 }
