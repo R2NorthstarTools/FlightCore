3c79b632fb771ebb6b142922fdb250bb58a1d5c1
GeckoEidechse <gecko.eidechse+git@pm.me>
2022-10-21 00:13:05 +0200

    feat: Empty skeleton page for ModsView
    Will be populated later with list of installed mods

---
+++ src-vue/src/views/ModsView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="fc__mods__container">
+        <h3>Installed Mods:</h3>
+        TODO
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "ModsView",
+});
+</script>
+
+<style scoped>
+.fc__mods__container {
+    padding: 20px 30px;
+    color: white;
+    position: relative;
+}
+</style>
