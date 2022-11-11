bd2c3b496df76499f1fdc1e579855a5b3d10da03
Remy Raes <raes.remy@gmail.com>
2022-11-12 00:40:58 +0100

    feat: add basic card layout for mods

--- src-vue/src/views/ThunderstoreModsView.vue
+++ src-vue/src/views/ThunderstoreModsView.vue
@@ -3,9 +3,19 @@
         <div v-if="mods.length === 0" class="fc__changelog__container">
             <el-progress :show-text="false" :percentage="50" :indeterminate="true" />
         </div>
-        <el-scrollbar v-else>
-            <el-card v-for="mod of mods" v-bind:key="mod.name">
-                <h1>{{ mod.name }}</h1>
+        <el-scrollbar v-else class="container">
+            <el-card v-for="mod of mods" v-bind:key="mod.name" :body-style="{ padding: '0px' }">
+                <img
+                    :src="mod.versions[0].icon"
+                    class="image"
+                />
+                <div style="padding: 14px 14px 10px 14px;">
+                    <span>{{ mod.name }}</span>
+                    <div class="desc">
+                        {{ mod.versions[0].description }}
+                    </div>
+                    <el-button type="primary" class="button">Install</el-button>
+                </div>
             </el-card>
         </el-scrollbar>
     </div>
@@ -28,7 +38,7 @@ export default defineComponent({
                     versions: [
                         {
                             description: "Regular scorch is replaced by Shrek from the smash hit cult classic, Shrek",
-                            icon: "https://gcdn.thunderstor…cons/UNO-Shrek-1.0.0.png",
+                            icon: "https://gcdn.thunderstore.io/live/repository/icons/UNO-Shrek-1.0.0.png",
                             version_number: "1.0.0",
                             download_url: "https://thunderstore.io/package/download/UNO/Shrek/1.0.0/",
                             date_created: "2022-11-10T00:03:13.057122Z"
@@ -44,11 +54,7 @@ export default defineComponent({
 });
 </script>
 
-<style>
-.el-scrollbar__view {
-    padding: 20px 30px;
-}
-
+<style scoped>
 .fc__changelog__container {
     padding: 20px 30px;
     position: relative;
@@ -62,7 +68,17 @@ export default defineComponent({
     user-select: none !important;
 }
 
-.el-card__body * {
-    max-width: 100%;
+.el-card {
+    display: inline-block;
+    max-width: 185px;
+}
+
+.desc {
+    font-size: 12px;
+    margin: 8px 0 16px;
+}
+
+button {
+    width: 100% !important;
 }
 </style>
