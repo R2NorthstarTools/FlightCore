ae04e8e981ccba051d09d91a96f78ba9f7713d25
GeckoEidechse <gecko.eidechse+git@pm.me>
2022-11-13 23:47:23 +0100

    refactor: Import all functions from lib
    So that I don't need to update the header each time I add/remove a
    function.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,12 +9,7 @@ use std::{
     time::Duration,
 };
 
-use app::{
-    check_is_flightcore_outdated, check_is_valid_game_path, check_northstar_running,
-    check_origin_running, convert_release_candidate_number, find_game_install_location,
-    get_enabled_mods, get_host_os, get_log_list, get_northstar_version_number,
-    install_northstar, launch_northstar, linux_checks_librs, GameInstall, NorthstarMod,
-};
+use app::*;
 
 mod github;
 use github::release_notes::get_northstar_release_notes;
