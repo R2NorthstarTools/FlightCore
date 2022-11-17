029fd0c26f41c979b98e1fa1f5ccbd747a3b07cb
Rémy Raes <contact@remyraes.com>
2022-11-17 06:46:56 +0100

    feat: cards container is responsive
    Mods cards will always appear centered on the screen, and cards container
    width adjusts to window width.

--- src-vue/src/views/ThunderstoreModsView.vue
+++ src-vue/src/views/ThunderstoreModsView.vue
@@ -4,56 +4,59 @@
             <el-progress :show-text="false" :percentage="50" :indeterminate="true" />
         </div>
         <el-scrollbar v-else class="container">
-            <!-- Search filters -->
-            <div class="filter_container">
-                <el-input v-model="input" placeholder="Search" clearable @input="onFilterTextChange" />
-            </div>
-
-            <!-- Message displayed if no mod matched searched words -->
-            <div v-if="filteredMods.length === 0 && input.length !== 0" class="noModFound">
-                No matching mod has been found.<br/>
-                Try another search!
-            </div>
-
-            <!-- Mod cards -->
-            <el-card v-for="mod of modsList" v-bind:key="mod.name" :body-style="{ padding: '0px' }">
-                <img
-                    :src="mod.versions[0].icon"
-                    class="image"
-                />
-                <div style="padding: 0px 10px 10px;">
-                    <span class="statContainer">
-                        <el-icon class="no-inherit">
-                            <Download />
-                        </el-icon>
-                        {{ modDownloadsCount(mod) }}
-                    </span>
-
-                    <span class="statContainer">
-                        {{ mod.rating_score }}
-                        <el-icon class="no-inherit">
-                            <Star />
-                        </el-icon>
-                    </span>
-                    <br/>
-
-                    <div class="name hide-text-overflow">{{ mod.name }}</div>
-                    <div class="author hide-text-overflow">by {{ mod.owner }}</div>
-                    <div class="desc">
-                        {{ mod.versions[0].description }}
-                    </div>
-                    <span style="display: flex">
-                        <el-button type="primary" style="flex: 6">
-                            {{ getModButtonText(mod) }}
-                        </el-button>
-                        <el-button link type="info" class="infoBtn" @click="openURL(mod.package_url)">
-                            <el-icon>
-                                <InfoFilled />
-                            </el-icon>
-                        </el-button>
-                    </span>
+            <div class="card-container">
+                <!-- Search filters -->
+                <div class="filter_container">
+                    <el-input v-model="input" placeholder="Search" clearable @input="onFilterTextChange" />
                 </div>
-            </el-card>
+
+                <!-- Message displayed if no mod matched searched words -->
+                <div v-if="filteredMods.length === 0 && input.length !== 0" class="noModFound">
+                    No matching mod has been found.<br/>
+                    Try another search!
+                </div>
+
+                <!-- Mod cards -->
+                <el-card v-for="mod of modsList" v-bind:key="mod.name" :body-style="{ padding: '0px' }">
+                    <img
+                        :src="mod.versions[0].icon"
+                        class="image"
+                    />
+                    <div style="padding: 0px 10px 10px;">
+                        <span class="statContainer">
+                            <el-icon class="no-inherit">
+                                <Download />
+                            </el-icon>
+                            {{ modDownloadsCount(mod) }}
+                        </span>
+
+                        <span class="statContainer">
+                            {{ mod.rating_score }}
+                            <el-icon class="no-inherit">
+                                <Star />
+                            </el-icon>
+                        </span>
+                        <br/>
+
+                        <div class="name hide-text-overflow">{{ mod.name }}</div>
+                        <div class="author hide-text-overflow">by {{ mod.owner }}</div>
+                        <div class="desc">
+                            {{ mod.versions[0].description }}
+                        </div>
+
+                        <span style="display: flex">
+                            <el-button type="primary" style="flex: 6">
+                                {{ getModButtonText(mod) }}
+                            </el-button>
+                            <el-button link type="info" class="infoBtn" @click="openURL(mod.package_url)">
+                                <el-icon>
+                                    <InfoFilled />
+                                </el-icon>
+                            </el-button>
+                        </span>
+                    </div>
+                </el-card>
+            </div>
         </el-scrollbar>
     </div>
 </template>
@@ -98,7 +101,7 @@ export default defineComponent({
         /**
          * This method is called each time search input is modified, and
          * filters mods matching the input string.
-         * 
+         *
          * This converts research string and all researched fields to
          * lower case, to match mods regardless of font case.
          */
@@ -111,8 +114,8 @@ export default defineComponent({
             const searchValue = value.toLowerCase();
 
             this.filteredMods = this.mods.filter((mod: ThunderstoreMod) => {
-                return mod.name.toLowerCase().includes(searchValue) 
-                    || mod.owner.toLowerCase().includes(searchValue) 
+                return mod.name.toLowerCase().includes(searchValue)
+                    || mod.owner.toLowerCase().includes(searchValue)
                     || mod.versions[0].description.toLowerCase().includes(searchValue);
             });
         },
@@ -124,7 +127,7 @@ export default defineComponent({
         openURL(url: string): void {
             shell.open(url);
         },
-        
+
         /**
          * This computes the total count of downloads of a given mod, by adding
          * download count of each of its releases.
@@ -203,5 +206,52 @@ export default defineComponent({
 .noModFound {
     color: white;
     margin: 20px 5px;
+}
+
+.card-container {
+    margin: 0 auto;
+}
+
+/* Card container dynamic size */
+@media (max-width: 1000px) {
+    .card-container {
+        width: 752px;
+    }
+}
+
+@media (max-width: 812px) {
+    .card-container {
+        width: 574px;
+    }
+}
+
+@media (max-width: 624px) {
+    .card-container {
+        width: 376px;
+    }
+}
+
+@media (min-width: 1000px) {
+    .card-container {
+        width: 940px;
+    }
+}
+
+@media (min-width: 1188px) {
+    .card-container {
+        width: 1128px;
+    }
+}
+
+@media (min-width: 1376px) {
+    .card-container {
+        width: 1316px;
+    }
+}
+
+@media (min-width: 1565px) {
+    .card-container {
+        width: 1505px;
+    }
 }
 </style>
