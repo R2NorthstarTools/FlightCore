fd4712accb319cd0084f7e97fea4d9d1299d8938
GeckoEidechse <40122905+GeckoEidechse@users.noreply.github.com>
2023-02-09 08:27:39 +0100

    feat: Add version number to user-agent (#173)
    * feat: Add version number to user-agent
    
    Uses a crate that allows for formatting at compile time.
    
    * fix: Add comment

--- src-tauri/src/constants.rs
+++ src-tauri/src/constants.rs
@@ -1,6 +1,9 @@
 // This file stores various global constants values
 
-pub const APP_USER_AGENT: &str = "R2NorthstarTools/FlightCore";
+use const_format::concatcp;
+
+// FlightCore user agent for web requests
+pub const APP_USER_AGENT: &str = concatcp!("FlightCore/", env!("CARGO_PKG_VERSION"));
 
 // URL of the Northstar masterserver
 pub const MASTER_SERVER_URL: &str = "https://northstar.tf";
