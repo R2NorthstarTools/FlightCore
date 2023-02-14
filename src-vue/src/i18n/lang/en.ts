f2d47f8db558ff0f49fcefde993735be19cfb6b0
Remy Raes <contact@remyraes.com>
2023-02-14 19:20:51 +0100

    feat: translate play view

--- src-vue/src/i18n/lang/en.ts
+++ src-vue/src/i18n/lang/en.ts
@@ -8,6 +8,14 @@ export default {
             update: 'Update',
             updating: 'Updating...',
             ready_to_play: 'Launch game'
-        }
+        },
+
+        unknown_version: "Unknown version",
+        see_patch_notes: "see patch notes",
+        players: "players",
+        servers: "servers",
+        unable_to_load_playercount: "Unable to load playercount",
+        northstar_running: "Northstar is running:",
+        origin_running: "Origin is running:"
     }
 };
--- src-vue/src/i18n/lang/fr.ts
+++ src-vue/src/i18n/lang/fr.ts
@@ -8,6 +8,14 @@ export default {
             update: 'Mettre à jour',
             updating: 'Mise à jour...',
             ready_to_play: 'Jouer'
-        }
+        },
+
+        unknown_version: "Version inconnue",
+        see_patch_notes: "voir les notes de version",
+        players: "joueurs",
+        servers: "serveurs",
+        unable_to_load_playercount: "Impossible de charger les statistiques",
+        northstar_running: "Northstar est en cours d'exécution :",
+        origin_running: "Origin est en cours d'exécution :"
     }
 };
