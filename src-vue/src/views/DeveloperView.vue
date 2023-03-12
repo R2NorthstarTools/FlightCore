82739f56b198054acbde53527e9eb39fa14f2602
GeckoEidechse <40122905+GeckoEidechse@users.noreply.github.com>
2023-03-12 15:55:36 +0100

    feat: Allow opening a window with various repair features (#129)
    * feat: Initial code for spawning repair window
    
    * refactor: Move disable all but core mods button
    to repair window
    
    * refactor: Rename function
    
    * refactor: Move button location
    
    Still in DeveloperView for now, will be moved to settings page later
    
    * feat: Add info banner
    
    * feat: Set repair window title
    
    * fix: Stop thread crash on duplicate window open
    
    Instead of calling an unwrap, handle failure to create window which is
    usually caused by spawning a second repair window although one already
    exists.
    
    * fix: Re-add accidentally removed lines from merge
    
    * fix: Set top padding to zero px
    
    As there's no menu bar in this view
    
    * feat: Close all windows when close bttn is clicked

--- src-vue/src/views/DeveloperView.vue
+++ src-vue/src/views/DeveloperView.vue
@@ -4,6 +4,7 @@
             <el-alert title="Warning" type="warning" :closable="false" show-icon>
                 This page is designed for developers. Some of the buttons here can break your Northstar install if you do not know what you're doing!
             </el-alert>
+
             <h3>Basic:</h3>
 
             <el-button type="primary" @click="disableDevMode">
@@ -36,8 +37,8 @@
 
             <h3>Repair:</h3>
 
-            <el-button type="primary" @click="disableAllModsButCore">
-                Disable all but core mods
+            <el-button type="primary" @click="createNewWindow">
+                Open Repair window
             </el-button>
 
             <el-button type="primary" @click="forceInstallNorthstar">
@@ -117,28 +118,6 @@ export default defineComponent({
         },
         async launchGameWithoutChecks() {
             this.$store.commit('launchGame', true);
-        },
-        async disableAllModsButCore() {
-            let game_install = {
-                game_path: this.$store.state.game_path,
-                install_type: this.$store.state.install_type
-            } as GameInstall;
-            await invoke("disable_all_but_core", { gameInstall: game_install }).then((message) => {
-                ElNotification({
-                    title: 'Success',
-                    message: "Disabled all mods but core",
-                    type: 'success',
-                    position: 'bottom-right'
-                });
-            })
-                .catch((error) => {
-                    ElNotification({
-                        title: 'Error',
-                        message: error,
-                        type: 'error',
-                        position: 'bottom-right'
-                    });
-                });
         },
         async getInstalledMods() {
             let game_install = {
@@ -259,6 +238,18 @@ export default defineComponent({
                 .finally(() => {
                     // Clear old notification
                     notification.close();
+                });
+        },
+        async createNewWindow() {
+            await invoke("open_repair_window")
+                .then((message) => { })
+                .catch((error) => {
+                    ElNotification({
+                        title: 'Error',
+                        message: error,
+                        type: 'error',
+                        position: 'bottom-right'
+                    });
                 });
         },
     }
