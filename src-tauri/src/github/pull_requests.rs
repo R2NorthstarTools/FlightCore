de5372323a873ffaa9e9171669a3cce856eb2613
GeckoEidechse <gecko.eidechse+git@pm.me>
2023-03-19 13:04:15 +0100

    refactor: Download launcher zip into memory and extract it from there into a temp folder then only copy out necessary files and delete the rest

--- src-tauri/src/github/pull_requests.rs
+++ src-tauri/src/github/pull_requests.rs
@@ -314,11 +314,18 @@ pub async fn apply_launcher_pr(
     // return Err(format!("The current directory is {}", path.display()));
 
     // get download link
-    let download_url = get_launcher_download_link(pull_request).await?;
+    let download_url = get_launcher_download_link(pull_request.clone()).await?;
 
-    // download
-    let download_directory = format!("{}/___flightcore-temp-download-dir/", game_install_path);
-    match std::fs::create_dir_all(download_directory.clone()) {
+    let archive = match download_zip_into_memory(download_url).await {
+        Ok(archive) => archive,
+        Err(err) => return Err(err.to_string()),
+    };
+
+    let extract_directory = format!(
+        "{}/___flightcore-temp-download-dir/launcher-pr-{}",
+        game_install_path, pull_request.number
+    );
+    match std::fs::create_dir_all(extract_directory.clone()) {
         Ok(_) => (),
         Err(err) => {
             return Err(format!(
@@ -328,33 +335,38 @@ pub async fn apply_launcher_pr(
         }
     };
 
-    match download_zip(download_url, download_directory.clone()).await {
-        Ok(_) => (),
-        Err(err) => return Err(err.to_string()),
-    };
+    let target_dir = std::path::PathBuf::from(extract_directory.clone()); // Doesn't need to exist
+    zip_extract::extract(io::Cursor::new(archive), &target_dir, true).unwrap();
 
-    // extract
-    let zip_extract_folder_name = unzip_launcher_zip(format!(
-        "{}/ns-dev-test-helper-temp-pr-files.zip",
-        download_directory.clone()
-    ));
-    fs::remove_file(format!(
-        "{}/ns-dev-test-helper-temp-pr-files.zip",
-        download_directory.clone()
-    ))
-    .unwrap();
-
-    // Copy downloaded folder to game install folder
-    match copy_dir_all(zip_extract_folder_name.clone(), game_install_path) {
-        Ok(_) => (),
-        Err(err) => {
-            return Err(format!("Failed copying files: {}", err));
-        }
+    // Copy only necessary files from temp dir
+    // Copy:
+    // - NorthstarLauncher.exe
+    // - Northstar.dll
+    let files_to_copy = vec!["NorthstarLauncher.exe", "Northstar.dll"];
+    for file_name in files_to_copy {
+        let source_file_path = format!("{}/{}", extract_directory, file_name);
+        let destination_file_path = format!("{}/{}", game_install_path, file_name);
+        match std::fs::copy(&source_file_path, &destination_file_path) {
+            Ok(_result) => (),
+            Err(err) => {
+                return Err(format!(
+                    "Failed to copy necessary file {} from temp dir: {}",
+                    file_name, err
+                ))
+            }
+        };
     }
 
-    // Delete old unzipped
-    fs::remove_dir_all(zip_extract_folder_name).unwrap();
-    std::fs::remove_dir_all(download_directory).unwrap();
+    // delete extract directory
+    match std::fs::remove_dir_all(&extract_directory) {
+        Ok(()) => (),
+        Err(err) => {
+            return Err(format!(
+                "Failed to delete temporary download directory: {}",
+                err
+            ))
+        }
+    }
 
     println!("All done with installing launcher PR");
     Ok(())
