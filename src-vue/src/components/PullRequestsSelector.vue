c0db2acd5645ef5d06d2e94758d5167078bf12a6
GeckoEidechse <gecko.eidechse+git@pm.me>
2023-03-20 11:35:03 +0100

    fix: Remove unused dependencies

--- src-vue/src/components/PullRequestsSelector.vue
+++ src-vue/src/components/PullRequestsSelector.vue
@@ -56,8 +56,6 @@
 import { defineComponent } from 'vue'
 import { PullRequestType } from '../../../src-tauri/bindings/PullRequestType';
 import { PullsApiResponseElement } from '../../../src-tauri/bindings/PullsApiResponseElement';
-import { invoke } from "@tauri-apps/api";
-import { ElNotification } from "element-plus";
 
 export default defineComponent({
     name: 'PullRequestsSelector',
