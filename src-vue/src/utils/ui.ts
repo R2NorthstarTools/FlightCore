5d600106e9e828e039d0a2daf8ee32279f4def36
Remy Raes <contact@remyraes.com>
2023-03-28 17:59:44 +0200

    feat: wrap ElNotification class in a showNotification method

---
+++ src-vue/src/utils/ui.ts
@@ -0,0 +1,17 @@
+import { ElNotification } from "element-plus";
+
+/**
+ * Displays content to the user in the form of a notification appearing on screen bottom right.
+ **/
+function showNotification(
+    title: string,
+    message: string = '',
+    type: 'success' | 'warning' | 'error' = 'success'
+) {
+    ElNotification({
+        title, message, type,
+        position: 'bottom-right'
+    });
+}
+
+export {showNotification};
