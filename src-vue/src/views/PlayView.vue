c0dd32e1f377eca963826779ced6834711466d8f
Rémy Raes <contact@remyraes.com>
2023-04-10 12:24:30 +0200

    refactor: put services container in a flex container, for it not to overlap play button

--- src-vue/src/views/PlayView.vue
+++ src-vue/src/views/PlayView.vue
@@ -31,42 +31,46 @@ export default defineComponent({
 </script>
 
 <template>
-    <div class="fc_launch__container">
-        <div class="fc_title">Northstar</div>
-        <div class="fc_northstar__version__container">
-            {{ northstarVersion === '' ? $t('play.unknown_version') : `v${northstarVersion}` }}
-            <div v-if="northstarVersion !== ''" class="fc_changelog__link" @click="showChangelogPage">
-                ({{ $t('play.see_patch_notes') }})
+    <div class="fc_global__container">
+        <div class="fc_launch__container">
+            <div class="fc_title">Northstar</div>
+            <div class="fc_northstar__version__container">
+                {{ northstarVersion === '' ? $t('play.unknown_version') : `v${northstarVersion}` }}
+                <div v-if="northstarVersion !== ''" class="fc_changelog__link" @click="showChangelogPage">
+                    ({{ $t('play.see_patch_notes') }})
+                </div>
+                <div v-if="playerCount >= 0" class="fc-stats__container">
+                    {{ playerCount }} {{ $t('play.players') }},
+                    {{ serverCount }} {{ $t('play.servers') }}
+                </div>
+                <div v-else="playerCount >= 0" class="fc-stats__container">
+                    {{ $t('play.unable_to_load_playercount') }}
+                </div>
             </div>
-            <div v-if="playerCount >= 0" class="fc-stats__container">
-                {{ playerCount }} {{ $t('play.players') }},
-                {{ serverCount }} {{ $t('play.servers') }}
-            </div>
-            <div v-else="playerCount >= 0" class="fc-stats__container">
-                {{ $t('play.unable_to_load_playercount') }}
+            <div>
+                <PlayButton />
             </div>
         </div>
-        <div>
-            <PlayButton />
-            <div v-if="$store.state.developer_mode" id="fc_services__status">
-                <div>
-                    <div class="fc_version__line">{{ $t('play.northstar_running') }}</div>
-                    <div class="fc_version__line fc_version__line__boolean"> {{ northstarIsRunning }}</div>
-                </div>
-                <div>
-                    <div class="fc_version__line">{{ $t('play.origin_running') }}</div>
-                    <div class="fc_version__line fc_version__line__boolean">{{ $store.state.origin_is_running }}</div>
-                </div>
+
+        <div v-if="$store.state.developer_mode" id="fc_services__status">
+            <div>
+                <div class="fc_version__line">{{ $t('play.northstar_running') }}</div>
+                <div class="fc_version__line fc_version__line__boolean"> {{ northstarIsRunning }}</div>
+            </div>
+            <div>
+                <div class="fc_version__line">{{ $t('play.origin_running') }}</div>
+                <div class="fc_version__line fc_version__line__boolean">{{ $store.state.origin_is_running }}</div>
             </div>
         </div>
     </div>
 </template>
 
 <style scoped>
-.fc_launch__container {
+.fc_global__container {
     margin: 50px 50px 30px 50px;
     position: fixed;
     bottom: 0;
+    display: flex;
 }
 
 /* Titles */
@@ -102,13 +106,11 @@ export default defineComponent({
     border-color: var(--el-color-primary);
 }
 
-
 #fc_services__status {
-    display: inline-block;
-    position: fixed;
-    padding: 10px 20px;
     color: #e8edef;
-    bottom: 43px;
+    align-self: end;
+    margin-bottom: 18px;
+    margin-left: 18px;
 }
 
 .fc_version__line {
