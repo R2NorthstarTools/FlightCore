R2NorthstarTools/FlightCore

commit 7d5e7b5b6b83ba9ab80f9be984c0d455b51b6502

fix: Replace `<=0` with `.is_empty()` (#244)

src-tauri/src/mod_management/mod.rs
@@ -328,7 +328,7 @@ pub async fn fc_download_mod_and_install(
     let mods_directory = format!("{}/R2Northstar/mods/", game_install.game_path);
 
     // Early return on empty string
-    if thunderstore_mod_string.len() == 0 {
+    if thunderstore_mod_string.is_empty() {
         return Err("Passed empty string".to_string());
     }
 
@@ -483,7 +483,7 @@ pub fn delete_thunderstore_mod(
         }
     }
 
-    if !(mod_folders_to_remove.len() > 0) {
+    if !mod_folders_to_remove.is_empty() {
         return Err(format!(
             "No mods removed as no Northstar mods matching {thunderstore_mod_string} were found to be installed."
         ));

src-tauri/src/repair_and_verify/mod.rs
@@ -84,7 +84,7 @@ pub fn get_log_list(game_install: GameInstall) -> Result<Vec<std::path::PathBuf>
         }
     }
 
-    if log_files.len() > 0 {
+    if !log_files.is_empty() {
         Ok(log_files)
     } else {
         Err("No logs found".to_string())

commit 33caaeea73c4b655f649981a5a550b5b5c57df1d

fix: Fix incorrect is_empty check
Fixes a regression introduced in #244 that prevented any Thunderstore
mods from being uninstalled

Closes #269

src-tauri/src/mod_management/mod.rs
@@ -480,7 +480,7 @@ pub fn delete_thunderstore_mod(
         }
     }
 
-    if !mod_folders_to_remove.is_empty() {
+    if mod_folders_to_remove.is_empty() {
         return Err(format!(
             "No mods removed as no Northstar mods matching {thunderstore_mod_string} were found to be installed."
         ));