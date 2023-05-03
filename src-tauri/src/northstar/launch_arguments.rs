25d32c0fe1f05ec3766bf4c433efa56d8b04488d
Remy Raes <contact@remyraes.com>
2023-05-03 19:00:35 +0200

    fix: don't throw if ns_startup_args.txt file does not exist

--- src-tauri/src/northstar/launch_arguments.rs
+++ src-tauri/src/northstar/launch_arguments.rs
@@ -18,12 +18,8 @@ pub fn get_launch_arguments(game_path: &str) -> Result<Vec<String>, ()> {
 
 #[tauri::command]
 pub fn set_launch_arguments(game_path: &str, arguments: Vec<String>) -> Result<(), String> {
-    // If file does not exist, we return an empty list
     let launch_args_path = format!("{}/ns_startup_args.txt", game_path);
-    if !std::path::Path::new(&launch_args_path).exists() {
-        return Err("Launch arguments file does not exist.".to_string());
-    }
-
+    
     match std::fs::write(launch_args_path.clone(), arguments.join(" ")) {
         Ok(..) => (),
         Err(_) => return Err("Failed to save launch arguments.".to_string()),
