147ecd5e6aaa2238d8cf21e129a590da4953cc31
Remy Raes <contact@remyraes.com>
2023-05-03 19:11:43 +0200

    docs: add documentation to Rust methods

--- src-tauri/src/northstar/launch_arguments.rs
+++ src-tauri/src/northstar/launch_arguments.rs
@@ -1,6 +1,8 @@
 #[tauri::command]
+/// This method loads arguments from the ns_startup_args.txt Northstar launch
+/// arguments files.
+/// If this file does not exist, this will return an empty array.
 pub fn get_launch_arguments(game_path: &str) -> Result<Vec<String>, ()> {
-    // If file does not exist, we return an empty list
     let launch_args_path = format!("{}/ns_startup_args.txt", game_path);
     if !std::path::Path::new(&launch_args_path).exists() {
         return Ok(vec![]);
@@ -17,6 +19,9 @@ pub fn get_launch_arguments(game_path: &str) -> Result<Vec<String>, ()> {
 }
 
 #[tauri::command]
+/// This method puts an array of arguments into the ns_startup_args.txt Northstar
+/// launch arguments files.
+/// If the ns_startup_args.txt file does not exist, this will create it.
 pub fn set_launch_arguments(game_path: &str, arguments: Vec<String>) -> Result<(), String> {
     let launch_args_path = format!("{}/ns_startup_args.txt", game_path);
     
