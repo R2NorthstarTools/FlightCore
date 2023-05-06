c228a9c002305e9ed00063a35c019ff8de3efa1b
GeckoEidechse <gecko.eidechse+git@pm.me>
2023-05-06 16:09:34 +0200

    refactor: Move function to different source file
    Move `get_enabled_mods` into the mod management module.

--- src-tauri/src/mod_management/mod.rs
+++ src-tauri/src/mod_management/mod.rs
@@ -8,7 +8,6 @@ use app::NorthstarMod;
 use serde::{Deserialize, Serialize};
 use std::{fs, io::Read, path::PathBuf};
 
-use app::get_enabled_mods;
 use app::GameInstall;
 
 #[derive(Debug, Clone)]
@@ -81,6 +80,31 @@ impl std::ops::Deref for TempFile {
     fn deref(&self) -> &Self::Target {
         &self.0
     }
+}
+
+/// Returns a serde json object of the parsed `enabledmods.json` file
+pub fn get_enabled_mods(game_install: &GameInstall) -> Result<serde_json::value::Value, String> {
+    let enabledmods_json_path = format!("{}/R2Northstar/enabledmods.json", game_install.game_path);
+
+    // Check for JSON file
+    if !std::path::Path::new(&enabledmods_json_path).exists() {
+        return Err("enabledmods.json not found".to_string());
+    }
+
+    // Read file
+    let data = match std::fs::read_to_string(enabledmods_json_path) {
+        Ok(data) => data,
+        Err(err) => return Err(err.to_string()),
+    };
+
+    // Parse JSON
+    let res: serde_json::Value = match serde_json::from_str(&data) {
+        Ok(result) => result,
+        Err(err) => return Err(format!("Failed to read JSON due to: {}", err)),
+    };
+
+    // Return parsed data
+    Ok(res)
 }
 
 /// Gets all currently installed and enabled/disabled mods to rebuild `enabledmods.json`
