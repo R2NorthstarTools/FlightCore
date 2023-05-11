bd369274abf34e2544838dd857dfc2649adca7f5
Remy Raes <contact@remyraes.com>
2023-05-11 19:12:44 +0200

    refactor: use `if let` syntax to catch error in set_launch_arguments method

--- src-tauri/src/northstar/launch_arguments.rs
+++ src-tauri/src/northstar/launch_arguments.rs
@@ -28,13 +28,10 @@ pub fn get_launch_arguments(game_path: &str) -> Result<Vec<String>, ()> {
 /// launch arguments files.
 /// If the ns_startup_args.txt file does not exist, this will create it.
 pub fn set_launch_arguments(game_path: &str, arguments: Vec<String>) -> Result<(), String> {
-    let launch_args_path = format!("{}/ns_startup_args.txt", game_path);
-    let write_result = std::fs::write(launch_args_path.clone(), arguments.join(" "));
-    
-    if write_result.is_ok() {
-        log::info!("Launch arguments updated.");
-        return Ok(());
-    } else {
+    let launch_args_path = format!("{}/ns_startup_args.txt", game_path);    
+    if let Err(_) = std::fs::write(launch_args_path.clone(), arguments.join(" ")) {
         return Err("Failed to save launch arguments.".to_string());
     }
+    log::info!("Launch arguments updated.");
+    return Ok(());
 }
