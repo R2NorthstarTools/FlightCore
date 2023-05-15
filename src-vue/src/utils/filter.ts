5ee7a99403f7b8ed6ef9a45ae6e5d1f635dde509
GeckoEidechse <gecko.eidechse+git@pm.me>
2023-05-15 12:26:31 +0200

    docs: Add comment explaining function in detail

--- src-vue/src/utils/filter.ts
+++ src-vue/src/utils/filter.ts
@@ -1,6 +1,12 @@
 /**
  * Implements a fuzzy filter
-*/
+ * Iterates through chars of `search_term` and checks if each char exists in consecutive order in `text`.
+ * For example, this means that `text="Gecko"` and `search_term="geo"` will return `true`
+ * but using `text="Gecko"` and `search_term="goe"` will return `false`
+ * 
+ * Implements a subset of "fuzzy string searching"
+ * https://en.wikipedia.org/wiki/Approximate_string_matching
+ */
 function fuzzy_filter(text: string, search_term: string): boolean {
     const lowercase_text = text.toLowerCase();
     const lowercase_search_term = search_term.toLowerCase();
