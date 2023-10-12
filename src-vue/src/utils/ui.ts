006f836d6e7a470dd36283892d63267c9f7dd9a7
Remy Raes <raes.remy@gmail.com>
2023-10-13 00:23:47 +0200

    feat: notifications are only registered if fired while app isn't focused

--- src-vue/src/utils/ui.ts
+++ src-vue/src/utils/ui.ts
@@ -4,6 +4,7 @@ import { store } from "../plugins/store";
 
 /**
  * Displays content to the user in the form of a notification appearing on screen bottom right.
+ * If the app is not focused when this is invoked, a notification is added to the notifications menu.
  **/
 function showNotification(
     title: string,
@@ -11,7 +12,10 @@ function showNotification(
     type: 'success' | 'warning' | 'error' | 'info' = 'success',
     duration: number = 4500
 ): NotificationHandle {
-    store.commit('addNotification', {title, text: message, type});
+    if (!document.hasFocus()) {
+        store.commit('addNotification', {title, text: message, type});
+    }
+
     return ElNotification({
         title, message, type, duration,
         position: 'bottom-right',
