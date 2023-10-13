0e4fc0a71a1400f1f03476e11aedcd0241c41eae
GeckoEidechse <gecko.eidechse+git@pm.me>
2023-10-13 11:53:50 +0200

    refactor: Move repair/verify related funcs to mod Moves functions related to game install path verification checks to dedicated module in an effort to clean up `main.rs`

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -122,7 +122,7 @@ fn main() {
             get_flightcore_version_number,
             northstar::get_northstar_version_number,
             check_is_northstar_outdated,
-            verify_install_location,
+            repair_and_verify::verify_install_location,
             platform_specific::get_host_os,
             northstar::install::install_northstar_wrapper,
             northstar::install::update_northstar,
@@ -266,18 +266,6 @@ async fn check_is_northstar_outdated(
     }
 }
 
-/// Checks if is valid Titanfall2 install based on certain conditions
-#[tauri::command]
-async fn verify_install_location(game_path: String) -> bool {
-    match check_is_valid_game_path(&game_path) {
-        Ok(()) => true,
-        Err(err) => {
-            log::warn!("{}", err);
-            false
-        }
-    }
-}
-
 /// Installs the specified mod
 #[tauri::command]
 async fn install_mod_wrapper(
@@ -417,17 +405,4 @@ pub struct NorthstarMod {
     pub thunderstore_mod_string: Option<String>,
     pub enabled: bool,
     pub directory: String,
-}
-
-/// Checks whether the provided path is a valid Titanfall2 gamepath by checking against a certain set of criteria
-pub fn check_is_valid_game_path(game_install_path: &str) -> Result<(), String> {
-    let path_to_titanfall2_exe = format!("{game_install_path}/Titanfall2.exe");
-    let is_correct_game_path = std::path::Path::new(&path_to_titanfall2_exe).exists();
-    log::info!("Titanfall2.exe exists in path? {}", is_correct_game_path);
-
-    // Exit early if wrong game path
-    if !is_correct_game_path {
-        return Err(format!("Incorrect game path \"{game_install_path}\"")); // Return error cause wrong game path
-    }
-    Ok(())
 }
