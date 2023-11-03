229972aec18e3a2f7e545c089da91ee7d2a2f3cf
Tristan Kennedy <tristan@taskinoz.com>
2023-11-03 13:54:11 +1100

    Fixed version convering to support double digits

--- src-tauri/src/util.rs
+++ src-tauri/src/util.rs
@@ -231,6 +231,12 @@ pub fn move_dir_all(
 pub fn convert_release_candidate_number(version_number: String) -> String {
     // This simply converts `-rc` to `0`
     // Works as intended for RCs < 10, e.g.  `v1.9.2-rc1`  -> `v1.9.201`
-    // Doesn't work for larger numbers, e.g. `v1.9.2-rc11` -> `v1.9.2011` (should be `v1.9.211`)
-    version_number.replace("-rc", "0").replace("00", "")
+    // The check below is to prevent `v1.9.2-rc11` -> `v1.9.2011` (should be `v1.9.221`)
+    let (_, split) = version_number.rsplit_once("-rc").unwrap();
+
+    if split.len() > 1 {
+        version_number.replace("-rc", "0").replace("0", "")
+    } else {
+        version_number.replace("-rc", "0").replace("00", "")
+    }
 }
