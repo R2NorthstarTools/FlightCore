9df8fb8acdb6f90ba423c7b592b5ac4001f19e4f
GeckoEidechse <gecko.eidechse+git@pm.me>
2024-12-23 20:21:04 +0100

    feat: Copy over more structure from main branch

--- src-vue/src/main.ts
+++ src-vue/src/main.ts
@@ -1,4 +1,18 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
+import en from "./i18n/lang/en.json";
+import fr from "./i18n/lang/fr.json";
+import da from "./i18n/lang/da.json";
+import de from "./i18n/lang/de.json";
+import es from "./i18n/lang/es.json";
+import pl from "./i18n/lang/pl.json";
+import ru from "./i18n/lang/ru.json";
+import it from "./i18n/lang/it.json";
+import zh_Hans from "./i18n/lang/zh_Hans.json";
 
-createApp(App).mount("#app");
+
+const app = createApp(App);
+
+
+
+app.mount('#app')
