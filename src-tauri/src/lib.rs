fa39b791fa5ef3c8dd891465a2b56582cb2088b0
GeckoEidechse <gecko.eidechse+git@pm.me>
2025-01-05 01:04:43 +0100

    fix: Remove sample greet function

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -27,19 +27,12 @@ pub struct NorthstarThunderstoreReleaseWrapper {
     value: NorthstarThunderstoreRelease,
 }
 
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
-            greet,
             development::install_git_main,
             github::compare_tags,
             github::get_list_of_tags,
