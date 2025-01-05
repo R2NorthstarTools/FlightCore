5614a73d747ae55c01b8cbafadd00352ea61e662
GeckoEidechse <gecko.eidechse+git@pm.me>
2025-01-05 01:34:17 +0100

    feat: Add entry for translation key
    Co-authored-by: Alystrasz <contact@remyraes.com>

--- src-vue/src/vuex-shim.d.ts
+++ src-vue/src/vuex-shim.d.ts
@@ -3,6 +3,7 @@ import { Store } from 'vuex'
 
 declare module '@vue/runtime-core' {
     interface ComponentCustomProperties {
-        $store: Store<FlightCoreStore>
+        $store: Store<FlightCoreStore>,
+        $t: (key: string, ...params: any[]) => string;
     }
 }
