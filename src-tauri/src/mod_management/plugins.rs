36912d19dc5880e253ae582a53abcd0cb2574d5a
GeckoEidechse <gecko.eidechse+git@pm.me>
2025-01-05 01:02:30 +0100

    fix: Disable plugin installation for now until I can figure out how to re-enable the dialog again

--- src-tauri/src/mod_management/plugins.rs
+++ src-tauri/src/mod_management/plugins.rs
@@ -1,10 +1,9 @@
-// use tauri::api::dialog::blocking::MessageDialogBuilder;
-// use tauri::api::dialog::{MessageDialogButtons, MessageDialogKind};
-
 /// Prompt on plugin
 /// Returns:
 /// - true: user accepted plugin install
 /// - false: user denied plugin install
 pub fn plugin_prompt() -> bool {
-    todo!()
+    log::error!("Plugin install cancelled");
+    // TODO handle plugin installation again
+    false
 }
