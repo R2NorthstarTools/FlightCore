5cab90ab65187e786e6aaa0397e880ab366859ff
GeckoEidechse <gecko.eidechse+git@pm.me>
2025-03-02 00:47:05 +0100

    feat: Use esnext as build target to allow for top-level await

--- src-vue/vite.config.ts
+++ src-vue/vite.config.ts
@@ -8,6 +8,10 @@ const host = process.env.TAURI_DEV_HOST;
 export default defineConfig(async () => ({
   plugins: [vue()],
 
+  build: {
+    target: 'esnext', // Needed for top-level await
+  },
+
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
