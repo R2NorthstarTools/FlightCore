ff45dfc0d1e587979020bc6d1819e1a274ce8387
NachosChipeados <103285866+NachosChipeados@users.noreply.github.com>
2025-04-06 12:20:59 -0400

    Update Vanilla+ name check

--- src-tauri/src/constants.rs
+++ src-tauri/src/constants.rs
@@ -27,7 +27,8 @@ pub const BLACKLISTED_MODS: [&str; 3] = [
 ];
 
 /// List of Thunderstoremods that have some specific install requirements that makes them different from standard mods
-pub const MODS_WITH_SPECIAL_REQUIREMENTS: [&str; 1] = ["NanohmProtogen-VanillaPlus"];
+/// Keeping old Vanilla+ name just in case
+pub const MODS_WITH_SPECIAL_REQUIREMENTS: [&str; 1] = ["NanohmProtogen-VanillaPlus", "NachosChipeados-VanillaPlus"];
 
 /// Order in which the sections for release notes should be displayed
 pub const SECTION_ORDER: [&str; 11] = [
